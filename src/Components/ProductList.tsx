a54b13e2b01628875aedf14013595e1a31e37e9c
yapkarhui2000 <yapkarhui2000@gmail.com>
2025-10-04 16:01:41 +0100

    Remove old Assets folder from src

--- src/Components/ProductList.tsx
+++ src/Components/ProductList.tsx
@@ -1,37 +1,43 @@
 type ContentAreaProps = {
-	itemList: Product[]
-	onAddtoBasket: (product: Product)=>void;
-}
+  itemList: Product[];
+  onAddtoBasket: (product: Product) => void;
+};
 
 type Product = {
-	id: number
-	name: string
-	price: number
-	category: string
-	quantity: number
-	rating: number
-	image_link: string
-}
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  quantity: number;
+  rating: number;
+  image_link: string;
+};
 
 export const ProductList = (props: ContentAreaProps) => {
-	return (
-		<div id="productList">
-			{props.itemList.map((item) => {
-				const outOfStock = item.quantity ===0;
-				return (
-					<div key={item.name} className="product">	
-						<div className="product-top-bar">
-							<h2>{item.name}</h2>
-							<p> £{item.price.toFixed(2)} ({item.rating}/5)</p>
-						</div>
-						<img src={"./src/Assets/Product_Images/" + item.image_link}></img>
-						<button value={item.id} 
-						onClick={()=>props.onAddtoBasket(item)}
-						disabled={outOfStock}>
-							{outOfStock?"Out of stock":"Add to basket"}</button>
-					</div>
-				)
-			})}
-		</div>
-	)
-}
+  return (
+    <div id="productList">
+      {props.itemList.map((item) => {
+        const outOfStock = item.quantity === 0;
+        return (
+          <div key={item.name} className="product">
+            <div className="product-top-bar">
+              <h2>{item.name}</h2>
+              <p>
+                {" "}
+                £{item.price.toFixed(2)} ({item.rating}/5)
+              </p>
+            </div>
+            <img src={"Assets/Product_Images/" + item.image_link}></img>
+            <button
+              value={item.id}
+              onClick={() => props.onAddtoBasket(item)}
+              disabled={outOfStock}
+            >
+              {outOfStock ? "Out of stock" : "Add to basket"}
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
